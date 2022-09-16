9a237f340bb82e94e098fe41e622239372b08bd8
leeCoderEng <nouraddine.ait.elhadj@gmail.com>
2022-09-16 17:04:35 +0100

    Upload avatar & Refactor

---
+++ backend/Focus-backend/src/main/java/com/webapps/Focus/service/IRoleService.java
@@ -0,0 +1,2 @@
+package com.webapps.Focus.service;public interface IRoleService {
+}
--- backend/Focus-backend/src/main/java/com/webapps/Focus/service/RoleServiceImpl.java
+++ backend/Focus-backend/src/main/java/com/webapps/Focus/service/RoleServiceImpl.java
@@ -2,18 +2,15 @@ package com.webapps.Focus.service;
 
 import com.webapps.Focus.dao.RoleRepository;
 import com.webapps.Focus.entities.role.Role;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+@AllArgsConstructor
 @Service
 public class RoleService {
+
     private RoleRepository roleRepository;
-
-    public RoleService(RoleRepository roleRepository) {
-        super();
-        this.roleRepository = roleRepository;
-    }
-
-
     public void saveRole(Role role) {
         this.roleRepository.save(role);
     }
